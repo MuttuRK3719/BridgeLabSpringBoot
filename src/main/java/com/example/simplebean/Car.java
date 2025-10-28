0255fe139c2c3cae0b5f81e9e49136fb191b5ad0
Muttu <kagalmutturaj@gmail.com>
2025-10-28 14:41:30 +0530

    Car project is completed using xml

---
+++ src/main/java/com/example/simplebean/Car.java
@@ -0,0 +1,7 @@
+package com.example.simplebean;
+
+public class Car {
+    public void start() {
+        System.out.println("Car is starting");
+    }
+}
---
+++ src/main/java/com/example/simplebean/Main.java
@@ -0,0 +1,12 @@
+package com.example.simplebean;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        Car car = (Car) context.getBean("car");
+        car.start();
+    }
+}
